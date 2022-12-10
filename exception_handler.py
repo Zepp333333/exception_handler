52f9333657bd0c3da548cd91193f9b15f1e145f8
zepp333 <sspytdev@gmail.com>
2022-12-10 09:50:47 +0100

    Exception handler initial code complete

---
+++ exception_handler.py
@@ -0,0 +1,92 @@
+import logging
+import os
+from collections import defaultdict
+from functools import wraps
+from typing import Any, Callable, Dict, Optional, Type, List, Protocol, TypeVar, Union, runtime_checkable
+
+UNSET_DEFAULT_RETURN = '__unset_default_return__'
+OCCURRENCE = 1
+MAX_REPEATED_EXCEPTIONS = os.getenv('EXCEPTION_HANDLER_MAX_REPEATED_EXCEPTIONS') or 1
+
+logger = logging.getLogger(__name__)
+
+F = TypeVar('F', bound=Callable[..., Any])
+
+
+@runtime_checkable
+class PassThroughHandler(Protocol):
+    def __call__(self, func: F, *args, **kwargs) -> F:
+        return func
+
+
+HandlerFuncType = Union[
+    Callable[[Any, ...], Any],
+    PassThroughHandler
+]
+
+
+class MaxRepeatedExceptionsError(Exception):
+    pass
+
+
+def exception_handler(
+    handlers: Dict[Type[Exception], HandlerFuncType],
+    default_return: Optional[Any] = UNSET_DEFAULT_RETURN,
+    max_repeated_exceptions: int = MAX_REPEATED_EXCEPTIONS,
+    occurred_exceptions: defaultdict[Type[Exception], List[int]] = None
+):
+    def decorate(func: Callable):
+
+        nonlocal max_repeated_exceptions
+        nonlocal occurred_exceptions
+
+        if occurred_exceptions is None:
+            occurred_exceptions = defaultdict(list)
+
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as exc:
+                exc_type = type(exc)
+                occurred_exceptions[exc_type].append(OCCURRENCE)
+
+                if is_exceeded_max_repeated_exceptions(exc_type, occurred_exceptions, max_repeated_exceptions):
+                    raise MaxRepeatedExceptionsError(
+                        f'Exception {exc_type} exceeded max allowed occurrences of {max_repeated_exceptions}'
+                    )
+                if handler := get_handler_for_exception(handlers, exc):
+                    logger.debug(f'Exception_handler: found handler {handler} for exception {repr(exc)}.')
+                    print(handler)
+                    return handler(
+                        exception_handler(
+                            handlers=handlers,
+                            default_return=default_return,
+                            max_repeated_exceptions=max_repeated_exceptions,
+                            occurred_exceptions=occurred_exceptions
+                        )(func), *args, **kwargs)
+
+                else:
+                    if default_return != UNSET_DEFAULT_RETURN:
+                        logger.debug(f'Returned default value: {default_return} instead of raising {repr(exc)}')
+                        return default_return
+                    raise exc
+
+        return wrapped
+
+    return decorate
+
+
+def get_handler_for_exception(
+    handlers: Dict[Type[Exception], HandlerFuncType],
+    exc: Exception
+) -> Optional[HandlerFuncType]:
+    return handlers.get(type(exc))
+
+
+def is_exceeded_max_repeated_exceptions(
+    exc: Type[Exception],
+    occurred_exception: defaultdict[Type[Exception], List[int]],
+    max_repeated_exceptions: int
+) -> bool:
+    return len(occurred_exception.get(exc, [])) == max_repeated_exceptions + 1
---
+++ tests/test_exception_handler.py
@@ -0,0 +1,130 @@
+import logging
+from typing import Callable, Dict, Type
+from unittest import mock
+
+import pytest
+
+from exception_handler import HandlerFuncType, exception_handler, get_handler_for_exception, MaxRepeatedExceptionsError
+
+logger = logging.getLogger(__name__)
+
+HANDLED_ATTRIBUTE_ERROR = 'handled_attribute_error'
+HANDLED_VALUE_ERROR = 'handled_value_error'
+HANDLED_LOGGING = 'handled_logging'
+DEFAULT_RETURN = 'default_return'
+
+
+class RepeatedError(Exception):
+    pass
+
+
+class RetryThisError(Exception):
+    pass
+
+
+class LogThisError(Exception):
+    pass
+
+
+def attribute_error_handler(_func: Callable):
+    return HANDLED_ATTRIBUTE_ERROR
+
+
+def logging_handler(func):
+    logger.warning(HANDLED_LOGGING)
+    return func()
+
+
+def retry_handler(func: Callable, *args, **kwargs):
+        return func(*args, **kwargs)
+
+
+test_handlers: Dict[Type[Exception], HandlerFuncType] = {
+    AttributeError: attribute_error_handler,
+    RetryThisError: retry_handler,
+    RepeatedError: retry_handler,
+    LogThisError: logging_handler
+}
+
+
+def test_get_handler_for_exception_positive():
+    assert get_handler_for_exception(test_handlers, AttributeError()) == attribute_error_handler
+
+
+def test_get_handler_for_exception_negative():
+    assert get_handler_for_exception(test_handlers, RuntimeError()) is None
+
+
+def test_exception_handler_no_exception():
+    @exception_handler(handlers=test_handlers)
+    def func():
+        return 512
+
+    assert func() == 512
+
+
+def test_exception_handler_have_handler_for_exception():
+    @exception_handler(handlers=test_handlers)
+    def func():
+        raise AttributeError()
+
+    assert func() == HANDLED_ATTRIBUTE_ERROR
+
+
+def test_exception_handler_have_handler_for_exception_func_throw_twice():
+    @exception_handler(handlers=test_handlers)
+    def func():
+        raise RepeatedError()
+
+    with pytest.raises(MaxRepeatedExceptionsError):
+        func()
+
+
+def test_exception_handler_have_no_handler():
+    @exception_handler(handlers=test_handlers)
+    def func():
+        raise RuntimeError()
+
+    with pytest.raises(RuntimeError):
+        func()
+
+
+def test_exception_handler_default_return():
+    @exception_handler(handlers=test_handlers, default_return=DEFAULT_RETURN)
+    def func():
+        raise RuntimeError()
+
+    assert func() == DEFAULT_RETURN
+
+
+def test_exception_handler_side_effect_handler(caplog):
+    caplog.at_level(logging.WARNING)
+
+    call_count = 0
+
+    @exception_handler(handlers=test_handlers)
+    def func():
+        nonlocal call_count
+        if call_count == 0:
+            call_count += 1
+            raise LogThisError
+        else:
+            raise AttributeError
+
+    assert func() == HANDLED_ATTRIBUTE_ERROR
+    assert HANDLED_LOGGING in caplog.messages[0]
+
+
+def test_exception_handler_two_different_exceptions():
+    call_count = 0
+
+    @exception_handler(handlers=test_handlers, max_repeated_exceptions=2)
+    def func():
+        nonlocal call_count
+        if call_count == 0:
+            call_count += 1
+            raise RetryThisError
+        else:
+            raise AttributeError
+
+    assert func() == HANDLED_ATTRIBUTE_ERROR
