2379f65e0a6125719d04a8b407068da2aea0d181
zepp333 <sspytdev@gmail.com>
2023-01-14 18:43:09 +0100

    removed old tests

--- tests/test_exception_handler.py
+++ tests/test_exception_handler.py
@@ -1,146 +1,187 @@
-import logging
 from collections import defaultdict
-from typing import Callable, Dict, Type
 
 import pytest
+from typing import Dict
 
-from exception_handler import HandlerFuncType, exception_handler, get_handler_for_exception, MaxRepeatedExceptionsError, \
-    is_exceeded_max_repeated_exceptions
-
-logger = logging.getLogger(__name__)
-
-HANDLED_ATTRIBUTE_ERROR = 'handled_attribute_error'
-HANDLED_LOGGING = 'handled_logging'
-DEFAULT_RETURN = 'default_return'
+from exception_handler import exception_handler, MaxRepeatedExceptionsError
 
 
-class RepeatedError(Exception):
+class TestException(Exception):
     pass
 
 
-class RetryThisError(Exception):
+class TestException2(Exception):
     pass
 
 
-class LogThisError(Exception):
-    pass
+def test_specific_handler_called():
+    # Test setup
+    handlers = {TestException: lambda _: 1}
+
+    @exception_handler(handlers=handlers)
+    def test_function():
+        raise TestException()
+
+    # function call
+    assert test_function() == 1
 
 
-def attribute_error_handler(_func: Callable):
-    return HANDLED_ATTRIBUTE_ERROR
+def test_default_value_returned():
+    # Test setup
+    handlers = {TestException: lambda x: x + 1}
+    default_return = 'default_return'
+
+    @exception_handler(handlers=handlers, default_return=default_return)
+    def test_function():
+        raise TestException2()
+
+    # function call
+    assert test_function() == 'default_return'
 
 
-def logging_handler(func):
-    logger.warning(HANDLED_LOGGING)
-    return func()
+def test_exception_raised_when_no_handler_or_default():
+    # Test setup
+    handlers = {TestException: lambda x: x + 1}
+
+    @exception_handler(handlers=handlers)
+    def test_function():
+        raise TestException2()
+
+    # function call
+    with pytest.raises(TestException2):
+        test_function()
 
 
-def retry_handler(func: Callable, *args, **kwargs):
-    return func(*args, **kwargs)
+def test_max_repeated_exception_error_raised():
+    # Test setup
+    handlers = {TestException: lambda _: 1}
+    max_repeated_exceptions = 1
 
+    @exception_handler(handlers=handlers, max_repeated_exceptions=max_repeated_exceptions)
+    def test_function():
+        raise TestException()
 
-test_handlers: Dict[Type[Exception], HandlerFuncType] = {
-    AttributeError: attribute_error_handler,
-    RetryThisError: retry_handler,
-    RepeatedError: retry_handler,
-    LogThisError: logging_handler
-}
-
-
-def test_get_handler_for_exception_positive():
-    assert get_handler_for_exception(test_handlers, AttributeError()) == attribute_error_handler
-
-
-def test_get_handler_for_exception_negative():
-    assert get_handler_for_exception(test_handlers, RuntimeError()) is None
-
-
-def test_exception_handler_no_exception():
-    @exception_handler(handlers=test_handlers)
-    def func():
-        return 512
-
-    assert func() == 512
-
-
-def test_exception_handler_have_handler_for_exception():
-    @exception_handler(handlers=test_handlers)
-    def func():
-        raise AttributeError()
-
-    assert func() == HANDLED_ATTRIBUTE_ERROR
-
-
-def test_exception_handler_have_handler_for_exception_func_throw_twice():
-    @exception_handler(handlers=test_handlers)
-    def func():
-        raise RepeatedError()
-
+    test_function()
+    # function call
     with pytest.raises(MaxRepeatedExceptionsError):
-        func()
+        test_function()
 
 
-def test_exception_handler_have_no_handler():
-    @exception_handler(handlers=test_handlers)
-    def func():
-        raise RuntimeError()
+def test_occurrences_tracked():
+    # Test setup
+    handlers = {TestException: lambda _: 1, TestException2: lambda _: 2}
 
-    with pytest.raises(RuntimeError):
-        func()
+    @exception_handler(handlers=handlers, max_repeated_exceptions=3)
+    def test_function():
+        raise TestException()
 
-
-def test_exception_handler_default_return():
-    @exception_handler(handlers=test_handlers, default_return=DEFAULT_RETURN)
-    def func():
-        raise RuntimeError()
-
-    assert func() == DEFAULT_RETURN
+    test_function()
+    test_function()
+    test_function()
+    with pytest.raises(MaxRepeatedExceptionsError):
+        test_function()
 
 
-def test_exception_handler_side_effect_handler(caplog):
-    caplog.at_level(logging.WARNING)
+def test_multiple_exception_types_and_handlers():
+    # Test setup
+    handlers = {TestException: lambda _:  1, TestException2: lambda _: 2}
 
-    call_count = 0
+    exceptions_count = 0
+    @exception_handler(handlers=handlers)
+    def test_function():
+        nonlocal exceptions_count
+        if exceptions_count == 0:
+            exceptions_count +=1
+            raise TestException()
+        if exceptions_count == 1:
+            exceptions_count +=1
+            raise TestException2
+        raise ValueError
 
-    @exception_handler(handlers=test_handlers)
-    def func():
-        nonlocal call_count
-        if call_count == 0:
-            call_count += 1
-            raise LogThisError
-        else:
-            raise AttributeError
+    # function call
+    assert test_function() == 1
 
-    assert func() == HANDLED_ATTRIBUTE_ERROR
-    assert HANDLED_LOGGING in caplog.messages[0]
+    assert test_function() == 2
 
+    with pytest.raises(ValueError):
+        test_function()
 
-def test_exception_handler_two_different_exceptions():
-    call_count = 0
+def test_nested_function_calls_and_exception_types():
+    # Test setup
+    handlers = {TestException: lambda _: 1}
 
-    @exception_handler(handlers=test_handlers, max_repeated_exceptions=2)
-    def func():
-        nonlocal call_count
-        if call_count == 0:
-            call_count += 1
-            raise RetryThisError
-        else:
-            raise AttributeError
+    @exception_handler(handlers=handlers)
+    def test_function():
+        def nested_function():
+            raise TestException()
 
-    assert func() == HANDLED_ATTRIBUTE_ERROR
+        return nested_function()
 
-
-def test_is_exceeded_max_repeated_exceptions_positive():
-    assert is_exceeded_max_repeated_exceptions(
-        ValueError,
-        defaultdict(list, {ValueError: [1, 1]}),
-        1
-    ) is True
+    # function call
+    assert test_function() == 1
 
 
-def test_is_exceeded_max_repeated_exceptions_negative():
-    assert is_exceeded_max_repeated_exceptions(
-        ValueError,
-        defaultdict(list, {ValueError: [1]}),
-        1
-    ) is False
+def test_keyword_arguments():
+    # Test setup
+
+    handlers = {TestException: lambda _, x, y: x+ y}
+
+    @exception_handler(handlers=handlers)
+    def test_function(x, y):
+        raise TestException()
+
+    # function call
+    assert test_function(x=1, y=2) == 3
+
+
+def test_non_exception_type():
+    # Test setup
+    handlers = {int: lambda x: x + 1}
+
+    @exception_handler(handlers=handlers)
+    def test_function():
+        raise TypeError()
+
+    # function call
+    with pytest.raises(TypeError):
+        test_function()
+
+
+def test_empty_handlers_dict():
+    # Test setup
+    handlers = {}
+
+    @exception_handler(handlers=handlers)
+    def test_function():
+        raise TestException()
+
+    # function call
+    with pytest.raises(TestException):
+        test_function()
+
+
+def test_none_default_return():
+    # Test setup
+    handlers = {TestException: lambda x: x + 1}
+    default_return = None
+
+    @exception_handler(handlers=handlers, default_return=default_return)
+    def test_function():
+        raise TestException2()
+
+    # function call
+    assert test_function() is None
+
+
+def test_empty_occurred_exceptions_list():
+    # Test setup
+    handlers = {TestException: lambda _: 1}
+    occurred_exceptions = defaultdict(list)
+
+    @exception_handler(handlers=handlers, seen_exceptions=occurred_exceptions)
+    def test_function():
+        raise TestException()
+
+    # function call
+    test_function()
+    assert occurred_exceptions == {TestException: [1]}
